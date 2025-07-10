17fe88a2048abe0ed0d7bbc890ea1cd835116a14
daridev <darideveloper@gmail.com>
2025-07-09 19:02:11 -0600

    feat (urls): setup media urls

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
 
@@ -9,27 +11,20 @@ from survey import views as survey_views
 
 # Setup drf router
 router = routers.DefaultRouter()
-router.register(
-    r'companies',
-    survey_views.CompanyViewSet,
-    basename='companies'
-)
+router.register(r"companies", survey_views.CompanyViewSet, basename="companies")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path("admin/", admin.site.urls),
     # Redirects
+    path("", RedirectView.as_view(url="/admin/"), name="home-redirect-admin"),
     path(
-        '',
-        RedirectView.as_view(url='/admin/'),
-        name='home-redirect-admin'
+        "accounts/login/",
+        RedirectView.as_view(url="/admin/"),
+        name="login-redirect-admin",
     ),
-    path(
-        'accounts/login/',
-        RedirectView.as_view(url='/admin/'),
-        name='login-redirect-admin'
-    ),
-    
     # API URLs
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
+
+if not settings.STORAGE_AWS:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
