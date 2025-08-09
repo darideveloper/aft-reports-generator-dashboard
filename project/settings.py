c3a83fa69c0342e615c1ad2d7b90ed377cad50fe
daridev <darideveloper@gmail.com>
2025-08-09 16:09:58 -0600

    chore (settings): remove duplicated BASE_DIR

--- project/settings.py
+++ project/settings.py
@@ -7,10 +7,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Setup .env file
 load_dotenv()
 ENV = os.getenv("ENV")
