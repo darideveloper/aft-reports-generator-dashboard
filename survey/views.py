96cfba0a89841ecc95b010265d100709d75d1890
daridev <darideveloper@gmail.com>
2025-09-16 11:48:28 -0600

    chore (survey/views): remove unreuired imports

--- survey/views.py
+++ survey/views.py
@@ -1,19 +1,9 @@
-import os
-import uuid
-
-from django.http import FileResponse, Http404
-from django.core.files import File
-from django.conf import settings
-from django.http import HttpResponse
-
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from utils import pdf_generator
 from utils.survey_calcs import SurveyCalcs
-from utils.screenshots import render_image_from_url
 
 from survey import serializers, models
 
