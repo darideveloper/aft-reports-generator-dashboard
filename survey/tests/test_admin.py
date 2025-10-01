850e755cd0bdf569a1e89b9935a543672724e784
daridev <darideveloper@gmail.com>
2025-10-01 14:20:39 -0600

    fix (survey/ReportAdminTestCase): add data to report before generate

--- survey/tests/test_admin.py
+++ survey/tests/test_admin.py
@@ -1,13 +1,13 @@
-from bs4 import BeautifulSoup
+import math
 
 from django.http import HttpResponse
 from django.core.management import call_command
 
+from bs4 import BeautifulSoup
+
 from core.tests_base.test_admin import TestAdminBase
 from core.tests_base.test_models import TestSurveyModelBase
-
 from survey import models as survey_models
-
 from utils.media import get_media_url
 
 
@@ -183,22 +183,53 @@ class ReportAdminTestCase(TestAdminBase, TestSurveyModelBase):
         self.report = self.create_report(
             survey=self.survey, participant=self.participant
         )
+        
+        # Load fixtures
+        call_command("apps_loaddata")
+        call_command("initial_loaddata")
+
+        # Set question gorup scores to same percentage
+        question_groups = survey_models.QuestionGroup.objects.all()
+        for question_group in question_groups:
+            question_group.survey_percentage = 100 / len(question_groups)
+            question_group.save()
+
+        # Create 10 questions in each question group
+        questions = []
+        options = []
+        for question_group in question_groups:
+            for _ in range(10):
+                question = self.create_question(question_group=question_group)
+                questions.append(question)
+                options.append(
+                    self.create_question_option(question=question, text="yes", points=1)
+                )
+
+        # Calculate correct answers
+        score = 90
+        correct_answers = math.floor(len(options) * score / 100)
+
+        # Set required correct answers
+        for question_index in range(correct_answers):
+            self.create_answer(
+                participant=self.participant, question_option=options[question_index]
+            )
 
     def test_search_bar(self):
         """Validate search bar working"""
 
         self.submit_search_bar(self.endpoint)
-        
+
     def __validate_disabled_see_report_btn(self, response: HttpResponse):
         """Validate disabled see report button
-        
+
         Args:
             response (HttpResponse): Response of the request
         """
-        
+
         # Validate http status
         self.assertEqual(response.status_code, 200)
-        
+
         # Validate link "Ver Reporte" disabled
         soup = BeautifulSoup(response.content, "html.parser")
         link = soup.select_one(".field-custom_links > a")
@@ -210,54 +241,54 @@ class ReportAdminTestCase(TestAdminBase, TestSurveyModelBase):
 
     def test_custom_links_see_report_pending(self):
         """Validate custom link "Ver Reporte" working for pending report"""
-        
+
         # Update report status
         self.report.status = "pending"
         self.report.save()
 
         # Validate response
         response = self.client.get(f"{self.endpoint}")
-        
+
         self.__validate_disabled_see_report_btn(response)
-        
+
     def test_custom_links_see_report_processing(self):
         """Validate custom link "Ver Reporte" working for processing report"""
-        
+
         # Update report status
         self.report.status = "processing"
         self.report.save()
-        
+
         # Validate response
         response = self.client.get(f"{self.endpoint}")
-        
+
         self.__validate_disabled_see_report_btn(response)
-        
+
     def test_custom_links_see_report_error(self):
         """Validate custom link "Ver Reporte" working for error report"""
-        
+
         # Update report status
         self.report.status = "error"
         self.report.save()
-        
+
         # Validate response
         response = self.client.get(f"{self.endpoint}")
-        
+
         self.__validate_disabled_see_report_btn(response)
-        
+
     def test_custom_links_see_report_completed(self):
         """Validate custom link "Ver Reporte" working for completed report"""
-        
+
         # create real report with command
         call_command("generate_next_report")
-        
+
         response = self.client.get(f"{self.endpoint}")
-        
-        report = survey_models.Report.objects.get(id=self.report.id)
-        pdf_url = get_media_url(report.pdf_file)
-        
+
+        self.report.refresh_from_db()
+        pdf_url = get_media_url(self.report.pdf_file)
+
         # Validate response
         self.assertEqual(response.status_code, 200)
-        
+
         # Validate link "Ver Reporte" disabled
         soup = BeautifulSoup(response.content, "html.parser")
         link = soup.select_one(".field-custom_links > a")
